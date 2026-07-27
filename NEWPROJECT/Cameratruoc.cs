5ee8cf979c85f40e1ef10e4ae61fb8efbc24ae65
Nguyen Van Viet <nviet1089 @gmail.com>
2026-07-27 23:12:37 +0700

    commit

--- NEWPROJECT/Cameratruoc.cs
+++ NEWPROJECT/Cameratruoc.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Globalization;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
 public class CameraTruoc : thietbido
 {
     public string mathietbicamera {get; set; } = string.Empty;
@@ -20,5 +23,18 @@ public class CameraTruoc : thietbido
     public void ConnectSerial()
     {
         Console.WriteLine($"Camera: Đang mo luong video tu cong serial...");
+        try
+        {
+            Mat testImage = new Mat(100, 100, DepthType.Cv8U, 3);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"[EmguCV OK] Khởi tạo Mat thành công! Kích thước: {testImage.Width}x{testImage.Height}");
+            Console.ResetColor();
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[EmguCV LỖI]: {ex.Message}");
+            Console.ResetColor();
+        }
     }
 }
